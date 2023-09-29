ef5cf55d3284009a1665970ead9b29ad391e3993
pollock94 <palok94@gmail.com>
2023-09-29 14:07:21 +0200

    added data_loader and model limits

---
+++ src/data_loader.py
@@ -0,0 +1,54 @@
+"""
+This file contains the DataLoader class for the JUSTICE model.
+"""
+
+import os
+import pandas as pd
+import numpy as np
+
+
+class DataLoader:
+
+    """
+    This class loads all the input data for the JUSTICE model.
+
+    """
+
+    def __init__(self):
+        """
+        This method initializes the DataLoader class.
+
+        """
+        # Get the current working directory
+        current_directory = os.path.dirname(os.path.realpath(__file__))
+        print(current_directory)
+        # Go up to the root directory of the project (two levels up)
+
+        root_directory = os.path.dirname(current_directory)
+        print(root_directory)
+
+        # Create the data file path
+        data_file_path = os.path.join(root_directory, "data")
+        print(data_file_path)
+
+        ###############################################################################
+        # Load the Economic data in pickle format
+        ###############################################################################
+
+        # Load GDP
+        with open(os.path.join(data_file_path, "gdp_dict.pickle"), "rb") as f:
+            self.GDP_DICT = pd.read_pickle(f)
+
+        # Load the population
+        with open(os.path.join(data_file_path, "population_dict.pickle"), "rb") as f:
+            self.POPULATION_DICT = pd.read_pickle(f)
+
+        # Load the capital stock initial values
+        with open(os.path.join(data_file_path, "capital_init_arr.pickle"), "rb") as f:
+            self.CAPITAL_INIT_ARRAY = pd.read_pickle(f)
+
+        # Load the Saving Rate initial values
+        with open(
+            os.path.join(data_file_path, "savings_rate_init_arr.pickle"), "rb"
+        ) as f:
+            self.SAVING_RATE_INIT_ARRAY = pd.read_pickle(f)
---
+++ src/model_limits.py
@@ -0,0 +1,21 @@
+"""
+This file contains the model limits for the JUSTICE model.
+
+"""
+
+
+class ModelLimits:
+    """
+    This class contains the model limits for the JUSTICE model.
+
+    """
+
+    # Lower limit for the model
+    self.output_lo = 0.0
+    self.gross_output_lo = 0.0
+    self.investment_lo = 0.0
+    self.consumption_lo = 0.0
+    self.capital_lo = 0.0
+    self.consumption_per_capita_lo = 0.0
+
+    # Upper limit for the model
