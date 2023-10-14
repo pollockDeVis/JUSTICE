8a83859f012e6bd2307751d72bab0109cb91124d
pollock94 <palok94@gmail.com>
2023-10-14 19:23:42 +0200

    implemented downscaler. Added file for kalkuhl and added default params

---
+++ src/damage/kalkuhl.py
@@ -0,0 +1,13 @@
+"""
+Kalkuhl damage function taken from RICE50. It is a recursive function with the following parameters:
+
+1. kw_DT:  `short_run_temperature_change_coefficient`. 
+
+2. kw_DT_lag: `lagged_short_run_temperature_change_coefficient`. 
+
+3. kw_TDT: `interaction_term_temperature_change_coefficient`. 
+
+4. kw_TDT_lag:  `lagged_interaction_term_temperature_change_coefficient`. 
+
+5. kw_T:  `temperature_dependent_coefficient`.
+"""
