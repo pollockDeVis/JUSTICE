645261dfd4b5fd8f97aa6723f4f8a6d5c4309926
pollock94 <palok94@gmail.com>
2023-10-16 13:32:14 +0200

    damage function works. Validated with older implementation. Older implementation validated with RICE50 temperatures and the temperatures produced by RICE50-FAIR integration

--- src/default_parameters.py
+++ src/default_parameters.py
@@ -51,6 +51,8 @@ class DamageDefaults:
                 "lagged_interaction_term_temp_change_coefficient": -0.000718,
                 # Temperature dependent coefficient (originally kw_T in GAMS)
                 "temperature_dependent_coefficient": -0.00675,
+                # Damage Window - Buffer to hold older temperature and new temperature
+                "damage_window": 2,
             },
             "DICE": {},
             "BURKE": {},
