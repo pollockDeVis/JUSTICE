Fixed damage calculation. Gross output to be implemented later

src/damage/kalkuhl.py
@@ -102,7 +102,7 @@ class DamageKalkuhl:
         )
 
         # Create damage to output array of shape (region_list, climate_ensembles)
-        self.damage_to_output = np.zeros(
+        self.total_damage_fraction = np.zeros(
             (
                 len(self.region_list),
                 self.climate_ensembles,
@@ -113,7 +113,7 @@ class DamageKalkuhl:
         """
         Returns the damage as a percentage for the given temperature and time step.
         Damages [Trill 2005 USD / year]
-        Damages can be calculated using output - (original) output * damage_to_output
+        Damages can be calculated using output - Damage = gross output * total_damage_fraction
         """
         # Assert that temperature is of shape (region_list, climate_ensembles)
         assert temperature.shape == (
@@ -187,14 +187,17 @@ class DamageKalkuhl:
                 self.damage_growth_rate,
             )
 
-            total_damage_fraction = unbounded_damage_fraction + gradient_damage_fraction
-            self.damage_to_output = 1 - total_damage_fraction
+            self.total_damage_fraction = (
+                unbounded_damage_fraction + gradient_damage_fraction
+            )
+            # total_damage_fraction = unbounded_damage_fraction + gradient_damage_fraction
+            # self.damage_to_output = 1 - total_damage_fraction
 
             # Update the first column of the temperature array and damage coefficient array for the next timestep
             self.temperature_array[:, 0, :] = self.temperature_array[:, 1, :]
             self.damage_coefficient[:, 0, :] = self.damage_coefficient[:, 1, :]
 
-        return self.damage_to_output
+        return self.total_damage_fraction
 
     def __getattribute__(self, __name: str) -> Any:
         """

src/economy/neoclassical.py
@@ -120,8 +120,15 @@ class NeoclassicalEconomyModel:
             (len(self.region_list), len(self.model_time_horizon), self.NUM_OF_ENSEMBLES)
         )
 
-        # Initializing the damages array
+        # TODO: Create a Gross Output array instead of damage fraction array
+
+        # Initializing the damage fraction array # TODO: Need to remove this
         self.damage_fraction = np.zeros(
+            (len(self.region_list), len(self.model_time_horizon), self.NUM_OF_ENSEMBLES)
+        )
+
+        # Initializing the damage array
+        self.damage = np.zeros(
             (len(self.region_list), len(self.model_time_horizon), self.NUM_OF_ENSEMBLES)
         )
 
@@ -265,9 +272,12 @@ class NeoclassicalEconomyModel:
         if timestep == 0 or timestep == 1:  # Damage is zero in the first timestep
             self.output[:, timestep, :] = self.output[:, timestep, :]
         else:
+            self.damage[:, timestep, :] = (
+                self.output[:, timestep, :] * self.damage_fraction[:, timestep, :]
+            )
             # Mutiplying damage to get Net Output # YGROSS(t,n) * (1 - DAMFRAC_UNBOUNDED(t,n))
             self.output[:, timestep, :] = (
-                self.output[:, timestep, :] * self.damage_fraction[:, timestep, :]
+                self.output[:, timestep, :] - self.damage[:, timestep, :]
             )
         # Subtract abatement from output
         self.output[:, timestep, :] = (
@@ -333,7 +343,7 @@ class NeoclassicalEconomyModel:
         return self.abatement
 
     def get_damages(self):
-        return self.damage_fraction
+        return self.damage
 
     def get_consumption_per_capita(self, scenario, savings_rate):
         # Assert if scenario is not within the range of 0 - 4