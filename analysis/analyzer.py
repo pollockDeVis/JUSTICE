664477dbd98e24217324fdadc081732ad5a4c814
pollock94 <palok94@gmail.com>
2023-10-26 23:26:26 +0200

    wrote the wrapper code and the analyzer.py to perform experiments. Doesn't work

--- analysis/analyzer.py
+++ analysis/analyzer.py
@@ -1,13 +1,111 @@
 """
 This module contains the uncertainty analysis for the JUSTICE model using EMA Workbench.
 """
+import numpy as np
+import os
 
+# EMA
 from ema_workbench import (
     Model,
     RealParameter,
-    ScalarOutcome,
-    Constant,
-    perform_experiments,
-    ema_logging,
     ArrayOutcome,
+    CategoricalParameter,
+    Policy,
+    ema_logging,
+    MultiprocessingEvaluator,
 )
+from ema_workbench.util.utilities import save_results, load_results
+
+ema_logging.log_to_stderr(ema_logging.INFO)
+
+# JUSTICE
+from src.enumerations import Scenario
+from src.util.EMA_model_wrapper import model_wrapper
+from src.model_time import TimeHorizon
+from src.data_loader import DataLoader
+
+# Instantiate the DataLoader class
+data_loader = DataLoader()
+# Instantiate the TimeHorizon class
+time_horizon = TimeHorizon(start_year=2015, end_year=2300, data_timestep=5, timestep=1)
+
+
+def get_mean_median_5th_95th(results):
+    mean_array = np.mean(results, axis=2)
+    median_array = np.median(results, axis=2)
+    fifth_percentile_array = np.percentile(results, 5, axis=2)
+    ninety_fifth_percentile_array = np.percentile(results, 95, axis=2)
+
+    # Return list of arrays
+    return [
+        fifth_percentile_array,
+        mean_array,
+        median_array,
+        ninety_fifth_percentile_array,
+    ]
+
+
+def perform_exploratory_analysis(number_of_experiments=10, filename=None, folder=None):
+    # scenario,
+    # savings_rate,
+    # emissions_control_rate,
+    # elasticity_of_marginal_utility_of_consumption,
+    # pure_rate_of_social_time_preference,
+    # inequality_aversion,
+
+    # Instantiate the model
+    model = Model("JUSTICE", function=model_wrapper)
+
+    # Speicify uncertainties
+    model.uncertainties = [
+        CategoricalParameter("scenario", (0, 1, 2, 3, 4, 5, 6, 7)),
+        RealParameter("elasticity_of_marginal_utility_of_consumption", 0.0, 2.0),
+        RealParameter("pure_rate_of_social_time_preference", 0.0, 0.020),
+        RealParameter("inequality_aversion", 0.0, 2.0),
+    ]
+
+    # Set model levers - has to be 2D array of shape (57, 286) 57 regions and 286 timesteps
+    model.levers = [
+        [
+            RealParameter("savings_rate", 0.05, 0.5)
+            for _ in range(len(data_loader.REGION_LIST))
+        ]
+        for _ in range(len(time_horizon.model_time_horizon))
+    ]
+
+    model.levers = [
+        [
+            RealParameter("emissions_control_rate", 0.00, 1.0)
+            for _ in range(len(data_loader.REGION_LIST))
+        ]
+        for _ in range(len(time_horizon.model_time_horizon))
+    ]
+
+    # Specify outcomes
+    model.outcomes = [
+        ArrayOutcome("net_economic_output"),
+        ArrayOutcome("consumption"),
+        ArrayOutcome("consumption_per_capita"),
+        ArrayOutcome("emissions"),
+        ArrayOutcome("global_temperature"),
+        ArrayOutcome("economic_damage"),
+        ArrayOutcome("abatement_cost"),
+        ArrayOutcome("disentangled_utility"),
+    ]
+
+    with MultiprocessingEvaluator(model) as evaluator:
+        results = evaluator.perform_experiments(
+            number_of_experiments, reporting_frequency=100
+        )
+
+        if filename != None:
+            file_name = f"results_open_exploration_{number_of_experiments}"
+
+        if folder is None:
+            target_directory = os.path.join(os.getcwd(), "data/output", file_name)
+
+        save_results(results, file_name=target_directory)
+
+
+if __name__ == "__main__":
+    perform_exploratory_analysis(number_of_experiments=10, filename=None, folder=None)
--- src/util/EMA_model_wrapper.py
+++ src/util/EMA_model_wrapper.py
@@ -1,3 +1,56 @@
 """
 This module wraps the model for uncertainty analysis experiments using EMA.
 """
+
+from src.model import JUSTICE
+from src.enumerations import *
+
+
+def model_wrapper(
+    scenario,
+    savings_rate,
+    emissions_control_rate,
+    elasticity_of_marginal_utility_of_consumption,
+    pure_rate_of_social_time_preference,
+    inequality_aversion,
+    economy_type=Economy.NEOCLASSICAL,
+    damage_function_type=DamageFunction.KALKUHL,
+    abatement_type=Abatement.ENERDATA,
+    welfare_function=WelfareFunction.UTILITARIAN,
+):
+    model = JUSTICE(
+        start_year=2015,
+        end_year=2300,
+        timestep=1,
+        scenario=scenario,
+        economy_type=economy_type,
+        damage_function_type=damage_function_type,
+        abatement_type=abatement_type,
+    )
+
+    model.run(savings_rate=savings_rate, emissions_control_rate=emissions_control_rate)
+    datasets = model.evaluate(
+        welfare_function=welfare_function,
+        elasticity_of_marginal_utility_of_consumption=elasticity_of_marginal_utility_of_consumption,
+        pure_rate_of_social_time_preference=pure_rate_of_social_time_preference,
+        inequality_aversion=inequality_aversion,
+    )
+    net_economic_output = datasets["net_economic_output"]
+    consumption = datasets["consumption"]
+    consumption_per_capita = datasets["consumption_per_capita"]
+    emissions = datasets["emissions"]
+    global_temperature = datasets["global_temperature"]
+    economic_damage = datasets["economic_damage"]
+    abatement_cost = datasets["abatement_cost"]
+    disentangled_utility = datasets["disentangled_utility"]
+
+    return (
+        net_economic_output,
+        consumption,
+        consumption_per_capita,
+        emissions,
+        global_temperature,
+        economic_damage,
+        abatement_cost,
+        disentangled_utility,
+    )
