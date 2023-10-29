aa04481a1b5d66b1d84715d762dec1e52ffbc382
pollock94 <palok94@gmail.com>
2023-10-29 08:55:54 +0100

    1000 experiments test with mean in hpc

--- hpc_run.py
+++ hpc_run.py
@@ -1,4 +1,4 @@
 from analysis.analyzer import perform_exploratory_analysis
 
 if __name__ == "__main__":
-    perform_exploratory_analysis(number_of_experiments=10000)
+    perform_exploratory_analysis(number_of_experiments=1000)
