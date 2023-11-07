2a598e5adc19ee52f937777155bb732fb390f774
pollock94 <palok94@gmail.com>
2023-11-07 17:45:55 +0100

    rearraged hpc files and added data processor module

---
+++ analysis/output_data_processor.py
@@ -0,0 +1,139 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+import pandas as pd
+
+
+from ema_workbench import load_results, ema_logging
+
+ema_logging.log_to_stderr(level=ema_logging.DEFAULT_LEVEL)
+
+
+def concatenate_data(number_of_runs):
+    # create a list of filenames to load from
+    filenames = [
+        "results_open_exploration_" + str(number_of_runs) + "_" + metric
+        for metric in ["5th", "median", "mean", "95th"]
+    ]
+
+    # Create arrays to store the data
+    temperature_array = []
+    damages_array = []
+    disentangled_utility_array = []
+    experiment_array = []
+
+    # loop through filenames and load each file
+    for filename in filenames:
+        filepath = "./data/output/" + filename
+        print(filepath)
+        # load the data
+        experiments, outcomes = load_results(filepath)
+        experiments = experiments.iloc[:, [0, 1, 2, -3, -2, -1]]
+        temp = outcomes["global_temperature"]
+        damages = outcomes["economic_damage"]
+        dis_util = outcomes["disentangled_utility"]
+
+        # Append the data to the arrays
+        # Append the data to the lists
+        temperature_array.append(temp)
+        damages_array.append(damages)
+        disentangled_utility_array.append(dis_util)
+        experiment_array.append(experiments)
+
+    # Convert the lists back to numpy arrays using concatenate
+    temperature_array = np.concatenate(temperature_array, axis=0)
+    damages_array = np.concatenate(damages_array, axis=0)
+    disentangled_utility_array = np.concatenate(disentangled_utility_array, axis=0)
+    experiment_array = np.concatenate(experiment_array, axis=0)
+
+    return (
+        temperature_array,
+        damages_array,
+        disentangled_utility_array,
+        experiment_array,
+    )
+
+    # temperature_array = np.array(temperature_array)
+    # damages_array = np.array(damages_array)
+    # disentangled_utility = np.array(disentangled_utility_array)
+    # experiment_array = np.array(experiment_array)
+
+
+def calculate_welfare(
+    experiments,
+    disentangled_utility,
+    time_horizon,
+):
+    timestep_list = np.arange(
+        0, len(time_horizon.model_time_horizon), time_horizon.timestep
+    )
+    pure_rate_of_social_time_preference = experiments[2]
+    inequality_aversion = experiments[1]
+    elasticity_of_marginal_utility_of_consumption = experiments[0]
+
+    # print(pure_rate_of_social_time_preference, inequality_aversion, elasticity_of_marginal_utility_of_consumption)
+    discount_rate = 1 / (
+        np.power(
+            (1 + pure_rate_of_social_time_preference),
+            (time_horizon.timestep * (timestep_list)),
+        )
+    )
+
+    # print(discount_rate)
+    disentangled_utility_summed = np.sum(disentangled_utility, axis=0)
+    # print(disentangled_utility_summed.shape)
+    disentangled_utility_powered = np.power(
+        disentangled_utility_summed,
+        (
+            (1 - elasticity_of_marginal_utility_of_consumption)
+            / (1 - inequality_aversion)
+        ),
+    )
+
+    disentangled_utility_regional_powered = np.power(
+        disentangled_utility,
+        (
+            (1 - elasticity_of_marginal_utility_of_consumption)
+            / (1 - inequality_aversion)
+        ),
+    )
+
+    welfare_utilitarian = np.sum(
+        (
+            np.divide(
+                disentangled_utility_powered,
+                (1 - elasticity_of_marginal_utility_of_consumption),
+            )
+            - 1
+        )
+        * discount_rate,
+        axis=0,
+    )
+
+    welfare_utilitarian_regional = np.sum(
+        (
+            np.divide(
+                disentangled_utility_regional_powered,
+                (1 - elasticity_of_marginal_utility_of_consumption),
+            )
+            - 1
+        )
+        * discount_rate,
+        axis=1,
+    )
+
+    return welfare_utilitarian_regional, welfare_utilitarian  #
+
+
+if __name__ == "__main__":
+    (
+        temperature_array,
+        damages_array,
+        disentangled_utility,
+        experiment_array,
+    ) = concatenate_data(1000)
+    # Save the data as pickle files
+    np.save("./data/output/temperature_array.npy", temperature_array)
+    np.save("./data/output/damages_array.npy", damages_array)
+    np.save("./data/output/disentangled_utility.npy", disentangled_utility)
+    np.save("./data/output/experiment_array.npy", experiment_array)
