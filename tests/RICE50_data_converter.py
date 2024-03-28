6c1c49e2fd2fc55f5ea1787d5ef1d41ecf529596
pollock94 <palok94@gmail.com>
2024-03-28 17:00:46 +0100

    added verification script and some util codes for testing

--- tests/RICE50_data_converter.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-
-def RICE50_regional_SSP_data_converter():
-    """
-    Convert regional data for each SSP scenario from RICE50 to a format that can be used in the model
-    """
